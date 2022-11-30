374300f173758edc904a2c27e2d311d81b65d062
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-11-30 10:35:17 +0100

    fix: use server url

--- utils/socket.ts
+++ utils/socket.ts
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 const url = "https://soundcheckgame-ts-ws-backend.fdrive.se"
 const devurl = "http://localhost:5000"
 
-const socket = io(devurl, {
+const socket = io(url, {
   path: '/ws',
   autoConnect: true,
   transports: ['websocket'],
