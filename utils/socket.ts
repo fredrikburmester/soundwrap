022a57155ed443ca73f5a9582e731c3f003e8125
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-12-01 12:15:03 +0100

    feat: enviroment variables

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,10 +1,13 @@
 // // @ts-nocheck
 import { io } from "socket.io-client";
+import { getEnvVars } from '../hooks/useEnviroment';
 
 const url = "https://soundcheckgame-ts-ws-backend.fdrive.se"
 const devurl = "http://localhost:5000"
 
-const socket = io(url, {
+const env = getEnvVars();
+
+const socket = io(env.wsurl, {
   path: '/ws',
   autoConnect: true,
   transports: ['websocket'],
