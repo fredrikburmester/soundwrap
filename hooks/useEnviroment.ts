cd4fa6978f901656bd9f1bb61594ef1268da0f9a
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-12-04 16:21:02 +0100

    fix: use computer ip

--- hooks/useEnviroment.ts
+++ hooks/useEnviroment.ts
@@ -16,7 +16,7 @@ interface IEnv {
 
 const ENV: IEnv = {
   development: {
-    wsurl: 'http://0.0.0.0:5000',
+    wsurl: 'http://192.168.0.103:5000',
   },
   preview: {
     wsurl: 'https://soundcheckgame-ts-ws-backend.fdrive.se',
